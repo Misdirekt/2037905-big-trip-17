3cc76dbfff04e4065029c08bb741bfa719a2de6d
Misdirekt <snegvlico@gmail.com>
2022-05-08 16:22:30 +0300

    Время разбивать камни

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
-import NewTaskButtonView from './view/new-task-button-view.js';
-import {render} from './render.js';
+import TripPresenter from './presenter/trip-presenter';
+import { render } from './render.js';
+import CostView from './view/cost-view.js';
+import FiltersView from './view/filters-view.js';
+import NewPointButtonView from './view/new-point-button-view.js';
+import RouteView from './view/route-view.js';
 
-const siteMainElement = document.querySelector('.main');
-const siteHeaderElement = siteMainElement.querySelector('.main__control');
+const tripPresenter = new TripPresenter();
 
-render(new NewTaskButtonView(), siteHeaderElement);
+const tripInfoElement = document.querySelector('.trip-info');
+const filtersElement = document.querySelector('.trip-controls__filters');
+const mainElement = document.querySelector('.trip-main');
+const eventsElement = document.querySelector('.trip-events');
 
+render(new RouteView(), tripInfoElement);
+render(new CostView(), tripInfoElement);
+render(new NewPointButtonView(), mainElement);
+render(new FiltersView(), filtersElement);
 
+tripPresenter.init(eventsElement);
